75d40879bbdeea3003fbe7a30fc2f49dc04a1bae
ZYAMUR <zeynepbzd04@gmail.com>
2024-03-26 13:35:58 +0300

    Pushing files after writing the TestUtil.java testes and rerun jacoco with full coverage of Util.java class. Done all the coverages (Util.java, Compute.java).

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -13,4 +13,35 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+  @Test
+  public void testOneArgument() {
+    assertFalse(c.compute(4));
+  }
+
+  @Test
+  public void testEvenNumberArguments() {
+    assertFalse(c.compute(0,2,4,6));
+  }
+
+  @Test
+  public void testArgumentIsZero() {
+    try {
+      c.compute(1, 0, 2);
+      fail("Expected a RuntimeException to be thrown");
+    } catch (RuntimeException e) {
+      // Test passes if RuntimeException is caught here
+    }
+  }
+
+  @Test
+  public void testSumDivisibleByArgument() {
+    assertTrue(c.compute(1, 2, 3, 4, 5, 6, 7));
+  }
+
+  @Test
+  public void testNoConditionSatisfied() {
+    boolean result = c.compute(2,4,7);
+    assertFalse(result);
+  }
+
 }
